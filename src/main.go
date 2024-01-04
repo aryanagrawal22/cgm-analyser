bb80c90b2ba407928dc498fcbbc5f3e248c3dad7
Aryan Agrawal <aryanagrawal20023@gmail.com>
2024-01-04 15:12:29 +0530

    Env: Deploy changes

--- src/main.go
+++ src/main.go
@@ -9,10 +9,10 @@ import (
 )
 
 func main() {
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+    // err := godotenv.Load(".env")
+    // if err != nil {
+    //     log.Fatal("Error loading .env file")
+    // }
 
     routes.InitRoutes()
 
